2df605fc87d7a8180ff6388939767359a1a9c1fa
Diego Ordonez - MBP <diegordonez4@gmail.com>
2024-09-04 10:21:15 +0200

    Uplad, list and download file from az storage

--- az/az-204/blob-client/blob-client/Program.cs
+++ az/az-204/blob-client/blob-client/Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+Console.WriteLine("Azure Blob Starge excercise\n");
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync().GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+static async Task ProcessAsync()
+{
+    //Connection String
+    string storageConnectionSting = "";
+
+    //Create a client that can authenticate with a connection string
+    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionSting);
+
+    //create a unique name for the container
+    string containerName = "az204" + Guid.NewGuid().ToString();
+
+    // create the container and return a container client objet
+    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+    Console.WriteLine("A container named '" + containerName + "' has been created. " +
+    "\nTake a minute and verify in the portal." +
+    "\nNext a file will be created and uploaded to the container.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    //create a local file in the ./data/ directory for uploading and downloading
+    string localPath = "./data/";
+    string fileName = "wtfile" + Guid.NewGuid().ToString() + ".txt";
+    string localFilePath = Path.Combine(localPath, fileName);
+
+    //write text to the file
+    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+    //Get a reference to the blob
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+    //Open the file and upload its data
+    using (FileStream uploadfileStream = File.OpenRead(localFilePath))
+    {
+        await blobClient.UploadAsync(uploadfileStream);
+        uploadfileStream.Close();
+    }
+
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" + 
+        " the blobs next.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    //list blobs in the container
+
+    Console.WriteLine("Listing blobs...");
+    await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+    {
+        Console.WriteLine("\t", blobItem.Name);
+    }
+
+    Console.WriteLine("\nYou can also verify by looking inside the " + 
+        "container in the portal." +
+        "\nNext the blob will be downloaded with an altered file name.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    //download the blob to a local file
+    // append the string "DOWNLOADED" before the .txt extension
+    string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOADED.txt");
+
+    Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+    // download the blob's contents and save it to a file
+    BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+    using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+    {
+        await download.Content.CopyToAsync(downloadFileStream);
+    }
+    Console.WriteLine("\nLocate the local file in the data directory created earlier to verify it was downloaded.");
+    Console.WriteLine("The next step is to delete the container and local files.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+}
